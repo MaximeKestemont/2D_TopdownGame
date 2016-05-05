20862818214d511ffcb44d2d584538ad10aeab2f
Maxime Kestemont <maxime.kestemont@realimpactanalytics.com>
2016-05-05 16:32:16 +0200

    ADD Win/Start scene

---
+++ Assets/Scripts/StartS.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class StartScene : MonoBehaviour 
+{
+    private void OnGUI()
+    {
+        Rect drawRect = new Rect(Screen.width * .05f, (Screen.height * .1f) -20.0f, Screen.width * .5f, Screen.height * .5f);
+        GUI.Box(drawRect, "");
+ 
+        drawRect.Set(drawRect.xMin + 50.0f, drawRect.yMin + 20.0f, drawRect.width, drawRect.height);
+        GUI.Label(drawRect, "Welcome to Breaking Rat Dude!");
+ 
+        drawRect.Set(drawRect.xMin, drawRect.yMin + 50.0f, drawRect.width, drawRect.height);
+        GUI.Label(drawRect, "Use the arrow keys to move. Avoid spiders, smash their webs.");
+ 
+        drawRect.Set(drawRect.xMin, drawRect.yMin + 50.0f, Screen.width * .25f, Screen.height * .25f);
+        if (GUI.Button(drawRect, "Play"))
+        {
+            Application.LoadLevel("Scene1");
+        }
+    }
+}
---
+++ Assets/Scripts/WinScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class WinScene : MonoBehaviour 
+{
+    private int destroyedWebs = 0;
+ 
+    // Use this for initialization
+    private void Start () 
+    {
+        destroyedWebs = PlayerPrefs.GetInt("DestroyedWebs");
+    }
+ 
+    private void OnGUI()
+    {
+        Rect drawRect = new Rect(Screen.width * .05f, (Screen.height * .1f) -20.0f, Screen.width * .5f, Screen.height * .5f);
+        GUI.Box(drawRect, "");
+ 
+        drawRect.Set(drawRect.xMin + 50.0f, drawRect.yMin + 20.0f, drawRect.width, drawRect.height);
+        GUI.Label(drawRect, "You WIN!");
+ 
+        drawRect.Set(drawRect.xMin, drawRect.yMin + 50.0f, drawRect.width, drawRect.height);
+        GUI.Label(drawRect, destroyedWebs + " webs destroyed!");
+ 
+        drawRect.Set(drawRect.xMin, drawRect.yMin + 50.0f, Screen.width * .25f, Screen.height * .25f);
+        if (GUI.Button(drawRect, "Play Again?"))
+        {
+            Application.LoadLevel("Scene1");
+        }
+    }
+}
